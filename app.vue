a9c30e889b637d1e352aa7403f216090ecf084df
calee <email@calee.tw>
2023-10-08 21:58:27 +0800

    init

---
+++ app.vue
@@ -0,0 +1,13 @@
+<template>
+  <CSSProvider :config="import('./master.css')">
+    <h1 class="font:40 font:heavy italic m:50 text:center">Hello World</h1>
+  </CSSProvider>
+</template>
+
+<script setup lang="ts">
+import { defineAsyncComponent } from "vue";
+
+const CSSProvider = defineAsyncComponent(
+  async () => (await import("@master/css.vue")).CSSProvider,
+);
+</script>
